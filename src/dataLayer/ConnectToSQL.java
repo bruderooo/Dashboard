5e3742567ee7ed5036416768e362ed4b2348f066
bruderooo <szymonjacon@gmail.com>
2020-06-09 19:16:34 +0200

    Repair ConnectToSQL class part 2.

--- src/dataLayer/ConnectToSQL.java
+++ src/dataLayer/ConnectToSQL.java
@@ -24,14 +24,6 @@ public class ConnectToSQL {
         );
         statement.execute("TRUNCATE TABLE routes;");
 
-        statement.execute("CREATE TABLE IF NOT EXISTS refuel (route_length float);");
-        statement.execute("TRUNCATE TABLE refuel;");
-        statement.execute("INSERT INTO refuel (route_length) VALUES (0);");
-
-        statement.execute("CREATE TABLE IF NOT EXISTS kilometrage (route_length float);");
-        statement.execute("TRUNCATE TABLE kilometrage;");
-        statement.execute("INSERT INTO kilometrage (route_length) VALUES (0);");
-
         connection.close();
     }
 
@@ -47,18 +39,6 @@ public class ConnectToSQL {
         statement.execute(
                 "INSERT INTO routes (start_date, end_date, route_length) " +
                     "VALUES (\"" + startDateString + "\", \"" + endDateString + "\", \"" + routeLength + "\");");
-
-        updateTable(statement, routeLength, "refuel");
-        updateTable(statement, routeLength, "kilometrage");
-
-        connection.close();
-    }
-
-    public void resetRefuel() throws SQLException {
-        Connection connection = DriverManager.getConnection(url, "root", "");
-        Statement statement = connection.createStatement();
-
-        statement.execute("UPDATE refuel SET route_length = 0");
 
         connection.close();
     }
